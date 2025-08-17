f0993f1cf2ec0b499119fb8113c76748f43df169
M-Adil-code <muhammadadeelkhan29@gmail.com>
2025-08-17 16:21:55 +0500

    Updating

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 let cardimg=document.querySelectorAll('.card-img-top')
+let sidebaricon=document.querySelector('.sidebaricon')
+let sidebarCrossIcon=document.querySelector('.cross')
 
 cardimg.forEach((item)=>{
 
@@ -12,4 +14,13 @@ cardimg.forEach((item)=>{
 		e.target.querySelector('.card-bgoverlay').classList.add('d-none')
 	})
 	
+})
+
+sidebaricon.addEventListener('click',()=>{
+	document.querySelector('.nav-bg').style.right='1px'
+	
+})
+
+sidebarCrossIcon.addEventListener('click',()=>{
+	document.querySelector('.nav-bg').style.right='-1000px'
 })
